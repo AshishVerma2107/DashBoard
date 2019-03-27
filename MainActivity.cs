fda2563c9e52dd24d1848db96857fb896e953792
Ashish Verma <45225991+AshishVerma2107@users.noreply.github.com>
2019-03-27 18:45:52 +0530

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Work121App.Adapter;
+
+namespace Work121App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        GridView gridView;
+      //  public static GridAdapter grid_adapter;
+        //InternetConnection con;
+        public static ImageAdapter image_adapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            gridView = FindViewById<GridView>(Resource.Id.gridview);
+            image_adapter = new ImageAdapter(this);
+
+           // grid_adapter = new GridAdapter(this, Utilities.imageList);
+            gridView.Adapter = image_adapter;
+           // gridView.NestedScrollingEnabled = false;
+           // gridView.ScrollbarFadingEnabled = true;
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Text;
+using Java.Util;
+using Work121App.Model;
+
+namespace Work121App
+{
+    public static class Utilities
+    {
+        public static string imageURL1 = "";
+        public static string imageName1 = "";
+        public static int currentPosition = -1;
+        public static string report_name = "";
+        public static string type = "";
+        public static int groupPosition = -1;
+        public static List<string> GlobalBusinessList = new List<string>();
+        public static List<ImageListModel> imageList = new List<ImageListModel>();
+
+        public static String fileName()
+        {
+            try
+            {
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").Format(new Date());
+                String name = "IMG_" + timeStamp + ".jpg";
+                return name;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public static byte[] GetStreamFromFile(string filePath)
+        {
+            try
+            {
+                Android.Net.Uri uri = Android.Net.Uri.FromFile(new Java.IO.File(filePath));
+                byte[] byteArray = System.IO.File.ReadAllBytes(uri.Path);
+                return byteArray;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
